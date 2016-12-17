cbe493456685b1443ef9d476f1f5d8d671c0e17f
Ryan Garant <ryan@aoausa.lan>
2016-12-16 16:26:59 -0800

    added inline styling with image handling capabilities

--- src/components/Wiki/Search.js
+++ src/components/Wiki/Search.js
@@ -20,13 +20,11 @@ export default class Search extends React.Component {
 	handleSubmit(event) {
 		event.preventDefault()
 		let text = this.state.value.replace(' ', '%20')
-		let url = `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${text}&utf8=&format=json`
+		let url = `https://crossorigin.me/https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${text}&utf8=&format=json`
 		axios.get(url)
 			.then(function(res) {
-				console.log(this.state)
-
-				this.setState({data: res.data.query.search})
-				console.log(typeof(this.state))
+				console.log(res)
+				// this.setState({data: res.data})
 			}.bind(this))
 			.catch(function(err) {
 				console.log(err)
@@ -39,6 +37,7 @@ export default class Search extends React.Component {
 				<form onSubmit={this.handleSubmit} action="" method="GET">
 					<input type="text" value={this.state.value || ''} onChange={this.handleChange} placeholder="Search..." />
 					<img className="icon" src={require('./magGlass.png')} onClick={this.handleSubmit} alt="search" width="20px" height="20px" />
+					<div id="one"></div>
 				</form>
 				<WikiArticle snippets={JSON.stringify(this.state.data)}/>
 			</div>
--- webpack.config.js
+++ webpack.config.js
@@ -21,18 +21,17 @@ module.exports = {
   module: {
     loaders: [
 	    {
-	      test: /\.jsx?$/,
+	      test: /\.js?$/,
 	      loaders: ['babel'],
-	      include: path.join(__dirname, 'src')
+	      include: path.join(__dirname, 'src'),
 	    },
-			{ test: /\.css$/, loader: 'style-loader!css-loader'},
-			{
-		    test: /\.(jpe?g|png|gif|svg)$/i,
-		    loaders: [
-		      'file?hash=sha512&digest=hex&name=[hash].[ext]',
-		      'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-		    ]
-		  }
+			{ 
+				test: /\.css$/, 
+				loader: 'style-loader!css-loader'},
+			{ 
+	    	test: /\.(png|jpg)$/,
+	    	loader: 'url'
+	    }
     ]
   }
 };
