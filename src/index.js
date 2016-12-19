83904849a0276fbc6cedcec0ce8ff2c1413e2b26
christRescues <ryantgarant@gmail.com>
2016-12-19 07:59:47 -0800

    adding react router functionality

--- src/index.js
+++ src/index.js
@@ -1,26 +1,34 @@
-import { AppContainer } from 'react-hot-loader';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/Wiki';
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ReactHotLoader from 'react-hot-loader'
+import { BrowserRouter } from 'react-router' 
+import App from './components/Wiki'
 
 const root = document.getElementById('root');
-ReactDOM.render(
-  <AppContainer>
-    <App />
-  </AppContainer>,
-  root
-);
 
 if (module.hot) {
-  module.hot.accept('./components/Wiki', () => {
-    // If you use Webpack 2 in ES modules mode, you can
-    // use <App /> here rather than require() a <NextApp />.
-    const NextApp = require('./components/Wiki').default;
+  module.hot.accept('./index.js')
+
+  module.hot.accept(
+  	'./components/Wiki', 
+  	() => { gNextApp = require('./components/Wiki').default;
     ReactDOM.render(
-      <AppContainer>
+      <ReactHotLoader>
+      	<BrowserRouter>
          <NextApp />
-      </AppContainer>,
+        </BrowserRouter>
+      </ReactHotLoader>,
       root
     );
   });
 }
+
+ReactDOM.render(
+  <ReactHotLoader>
+  	<BrowserRouter>
+    	<App />
+    </BrowserRouter>
+  </ReactHotLoader>,
+  root
+);
