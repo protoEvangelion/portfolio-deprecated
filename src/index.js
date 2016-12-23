982af1c1c2fb2c1ea78d56612716f97061a20811
Ryan Garant <ryan@aoausa.lan>
2016-12-23 12:03:16 -0800

    split off routes into their own stateless component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { BrowserRouter, Match, Miss } from 'react-router' 
-import Wiki from './components/Wiki'
-import Home from './components/Home'
-import NotFound from './components/NotFound'
-
+import Routes from './router/Routes.js'
+// Redux imports 
 import { createStore, applyMiddleware, combineReducers } from 'redux'
 import { Provider } from 'react-redux'
 import thunk from 'redux-thunk'
 import * as reducers from './store/reducers'
+
 
 const store = createStore(combineReducers(reducers), applyMiddleware(thunk))
 
@@ -16,13 +14,7 @@ const root = document.getElementById('root');
 
 export const Root = () => (
 	<Provider store={store}>
-		<BrowserRouter>
-			<div>
-				<Match exactly pattern="/" component={Home} />
-				<Match pattern="/wikiViewer" component={Wiki} />
-				<Miss component={NotFound} />
-			</div>
-		</BrowserRouter>
+		<Routes />
 	</Provider>
 )
 
