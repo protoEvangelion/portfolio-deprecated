384c922a5eb953df1b0f1191faeacd081bca705f
christRescues <ryantgarant@gmail.com>
2017-01-09 22:12:41 -0800

    pulled city from reverse geolocation lookup and funneled into fetchweather action

--- client/components/Weather/MyArea.js
+++ client/components/Weather/MyArea.js
@@ -1,19 +1,35 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import { fetchGeoData } from '../../actions/fetchGeoData'
+import { fetchWeather } from '../../actions/fetchWeather'
 import { bindActionCreators } from 'redux'
 import axios from 'axios'
 
 class MyArea extends Component {
 	constructor(props) {
 		super(props)
+		this.state = {geo: ''}
 		this.onBtnClick = this.onBtnClick.bind(this)
 	}
 	onBtnClick() {
+	  console.log('hellow')
 	  navigator.geolocation.getCurrentPosition(position => {
   		const lat = position.coords.latitude
   		const lng = position.coords.longitude
-  		this.props.fetchGeoData(lat, lng)
+  		const ROOT_URL = 'https://maps.googleapis.com/maps/api/geocode/json?sensor=true&latlng='
+  		const reqUrl = `${ROOT_URL}${lat},${lng}`
+			const url = `http://localhost:3000/api?url=${reqUrl}`
+			console.log('hello')
+			axios.get(url)
+				.then(response => {
+					const address = response.data.results[0].formatted_address
+					const end = address.slice(address.indexOf(',') + 2)
+					const city = end.slice(0, end.indexOf(','))
+
+					this.props.fetchWeather(city)
+				})
+				.catch(err => {
+					console.log(err)
+				})
 	  })
 	}
 	render() {
@@ -27,7 +43,7 @@ class MyArea extends Component {
 }
 
 function mapDispatchToProps(dispatch) {
-	return bindActionCreators({ fetchGeoData }, dispatch)
+	return bindActionCreators({ fetchWeather }, dispatch)
 }
 
 export default connect(null, mapDispatchToProps)(MyArea)
