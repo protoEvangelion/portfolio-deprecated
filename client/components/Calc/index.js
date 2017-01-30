2b765e470003f35c6e7ea0e377eafaae314e8dc5
christRescues <ryantgarant@gmail.com>
2017-01-30 07:59:45 -0800

    added scroll functionality to containers

--- client/components/Calc/index.js
+++ client/components/Calc/index.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {Component} from 'react'
 import Calc from './Calc'
+import smoothScroll from '../../helpers/scroll'
 
-const App = () => {
-  return (
-    <Calc />
-  )
+class App extends Component {
+  componentDidMount() {
+    smoothScroll(document.getElementById("calcContainer"))
+	}
+  render() {
+    return (
+      <div id="calcContainer" style={{marginBottom: '500px'}}>
+        <Calc />
+      </div>      
+    )
+  }
 }
 
 export default App
