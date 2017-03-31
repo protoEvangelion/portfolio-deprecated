29d4a9dfea1b60e21c04496ab28b7cbd9eb3083e
christRescues <ryantgarant@gmail.com>
2017-03-30 21:41:40 -0700

    major overhaul redux ssr with server side and client side reloading

---
+++ src/client.js
@@ -0,0 +1,35 @@
+import 'babel-polyfill'
+import React from 'react'
+import { render } from 'react-dom'
+import { Provider } from 'react-redux'
+import { AppContainer } from 'react-hot-loader'
+import createHistory from 'history/createBrowserHistory'
+import { ConnectedRouter } from 'react-router-redux'
+import configureStore from 'store/configure'
+
+import AppRoutes from 'routes'
+
+// eslint-disable-next-line no-underscore-dangle
+const initialState = window.__INITIAL_STATE__
+const history = createHistory()
+const store = configureStore(initialState, history)
+const root = document.getElementById('app')
+
+const renderApp = () => (
+  <AppContainer>
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <AppRoutes />
+      </ConnectedRouter>
+    </Provider>
+  </AppContainer>
+)
+
+render(renderApp(), root)
+
+if (module.hot) {
+  module.hot.accept('routes', () => {
+    require('routes')
+    render(renderApp(), root)
+  })
+}
---
+++ src/config.js
@@ -0,0 +1,28 @@
+const merge = require('lodash/merge')
+
+const browser = typeof window !== 'undefined'
+const ip = process.env.IP || 'localhost'
+const port = process.env.PORT || 3000
+const basename = `/${process.env.PUBLIC_PATH || ''}`.replace('//', '/')
+
+const config = {
+  all: {
+    env: process.env.NODE_ENV || 'development',
+    baseUrl: `http://${ip}:${port}${basename}`,
+    apiUrl: 'https://jsonplaceholder.typicode.com',
+    basename,
+    browser,
+    ip,
+    port,
+  },
+  test: {},
+  development: {},
+  production: {
+    ip: process.env.IP || 'localhost',
+    port: process.env.PORT || 8080,
+    baseUrl: 'https://ryantg.herokuapp.com',
+    apiUrl: 'https://jsonplaceholder.typicode.com',
+  },
+}
+
+module.exports = merge(config.all, config[config.all.env])
