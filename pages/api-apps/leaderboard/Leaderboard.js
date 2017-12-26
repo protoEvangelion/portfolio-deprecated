a8caa53d491c22b8a794aa67585177aa578a2190
protoEvangelion <ryantgarant@gmail.com>
2017-12-25 17:02:17 -0800

    Cleaned up folder structure

--- pages/api-apps/leaderboard/Leaderboard.js
+++ pages/api-apps/leaderboard/Leaderboard.js
@@ -1,5 +1,5 @@
+import { connectRedux } from '~/store'
 import React from 'react'
-import Radium from 'radium'
 
 const styles = {
   text: {
@@ -14,13 +14,11 @@ const styles = {
     fontFamily: "'Grand Hotel', cursive",
     fontSize: '3em',
     textAlign: 'center',
-  }
+  },
 }
 
-let Header = () => {
-  return (
-    <h3 style={styles.text}>freeCodeCamp Super Cool People Leaderboard</h3>
-  )
+const Leaderboard = () => {
+  return <h3 style={styles.text}>freeCodeCamp Super Cool People Leaderboard</h3>
 }
 
-export default Header = Radium(Header)
+export default connectRedux()(Leaderboard)
