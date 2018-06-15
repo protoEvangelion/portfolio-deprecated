ae1d32baf4c07e6b4dff0096ae23aee0ccd85307
protoEvangelion <ryantgarant@gmail.com>
2018-06-14 22:44:22 -0700

    Add grommet design & development framework

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -8,11 +8,14 @@ exports.layouts = {
 
 exports.components = {
   "component---cache-dev-404-page-js": preferDefault(require("/home/ryan/dev/portfolio/.cache/dev-404-page.js")),
-  "component---src-pages-index-js": preferDefault(require("/home/ryan/dev/portfolio/src/pages/index.js"))
+  "component---src-pages-index-js": preferDefault(require("/home/ryan/dev/portfolio/src/pages/index.js")),
+  "component---src-pages-404-js": preferDefault(require("/home/ryan/dev/portfolio/src/pages/404.js"))
 }
 
 exports.json = {
   "layout-index.json": require("/home/ryan/dev/portfolio/.cache/json/layout-index.json"),
   "dev-404-page.json": require("/home/ryan/dev/portfolio/.cache/json/dev-404-page.json"),
-  "index.json": require("/home/ryan/dev/portfolio/.cache/json/index.json")
+  "index.json": require("/home/ryan/dev/portfolio/.cache/json/index.json"),
+  "404.json": require("/home/ryan/dev/portfolio/.cache/json/404.json"),
+  "404-html.json": require("/home/ryan/dev/portfolio/.cache/json/404-html.json")
 }
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,10 +1,22 @@
+const autoprefixer = require('autoprefixer')
+
 module.exports = {
-	siteMetadata: {
-		title: `Ryan's Personal Portfolio`,
-	},
-	plugins: [
-		'gatsby-plugin-react-helmet',
-		'gatsby-plugin-react-next',
-		'gatsby-plugin-resolve-src',
-	],
+  siteMetadata: {
+    title: "Ryan's Personal Portfolio",
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-react-next',
+    'gatsby-plugin-resolve-src',
+    {
+      resolve: 'custom-sass-loader',
+      options: {
+        postCssPlugins: [
+          autoprefixer({
+            browsers: ['last 2 versions'],
+          }),
+        ],
+      },
+    },
+  ],
 }
