5ef6f5e9fb4c291286f99af8a8865fa24091f0f8
protoEvangelion <ryantgarant@gmail.com>
2018-06-23 19:52:23 -0700

    Add Typescript

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
     'gatsby-plugin-resolve-src',
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
+    'gatsby-plugin-typescript',
     {
       resolve: 'gatsby-source-filesystem',
       options: {
